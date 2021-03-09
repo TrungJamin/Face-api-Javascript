133003acd1088a3bced606423734ac898d80fbb5
TrungJamin <68997072+TrungJamin@users.noreply.github.com>
2021-03-09 19:08:01 +0700

    Upload Project

--- script.js
+++ script.js
@@ -1,52 +1,83 @@
-const imageUpload = document.getElementById('imageUpload')
+const imageUpload = document.getElementById("imageUpload");
 
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(start)
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+]).then(start);
 
 async function start() {
-  const container = document.createElement('div')
-  container.style.position = 'relative'
-  document.body.append(container)
-  const labeledFaceDescriptors = await loadLabeledImages()
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-  let image
-  let canvas
-  document.body.append('Loaded')
-  imageUpload.addEventListener('change', async () => {
-    if (image) image.remove()
-    if (canvas) canvas.remove()
-    image = await faceapi.bufferToImage(imageUpload.files[0])
-    container.append(image)
-    canvas = faceapi.createCanvasFromMedia(image)
-    container.append(canvas)
-    const displaySize = { width: image.width, height: image.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+  const container = document.createElement("div");
+  container.style.position = "relative";
+  document.body.append(container);
+  // Lấy toàn bộ dữ liệu gương mặt của các user
+  const labeledFaceDescriptors = await loadLabeledImages();
+
+  // Độ chính xác 60%
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+  let image;
+  let canvas;
+  console.log(faceMatcher);
+  document.body.append("Loaded");
+  imageUpload.addEventListener("change", async () => {
+    if (image) image.remove();
+    if (canvas) canvas.remove();
+
+    console.log(imageUpload.files[0]);
+
+    image = await faceapi.bufferToImage(imageUpload.files[0]);
+    console.log(image);
+    container.append(image);
+    canvas = faceapi.createCanvasFromMedia(image);
+    container.append(canvas);
+    const displaySize = { width: image.width, height: image.height };
+    faceapi.matchDimensions(canvas, displaySize);
+
+    // Xác thực các nhân vật trong ảnh chọn lên
+    const detections = await faceapi
+      .detectAllFaces(image)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+    console.log(detections);
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+    console.log(resizedDetections);
+    // Bước nhận diện
+    const results = resizedDetections.map((d) => {
+      console.log(faceMatcher.findBestMatch(d.descriptor)); // {_label: "Tony Stark", _distance: 0.5715871892946531} label => user id
+      return faceMatcher.findBestMatch(d.descriptor);
+    });
+    // Trả về kết quả các gương mặt quét được trong ảnh
+    console.log(results);
     results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-      drawBox.draw(canvas)
-    })
-  })
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: result.toString(),
+      });
+      drawBox.draw(canvas);
+    });
+  });
 }
 
 function loadLabeledImages() {
-  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  const labels = ["Anh Duc", "Hariwon", "Tran Thanh", "Trinh Thang Binh"];
+  // *Tìm hiểu về Promise.all();*
   return Promise.all(
-    labels.map(async label => {
-      const descriptions = []
+    labels.map(async (label) => {
+      const descriptions = [];
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
-        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-        descriptions.push(detections.descriptor)
+        const img = await faceapi.fetchImage(
+          `https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`
+        );
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+        console.log(detections);
       }
-
-      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+      console.log(descriptions);
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
     })
-  )
+  );
 }
